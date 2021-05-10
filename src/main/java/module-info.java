a3f4ffd763341730aee1f065e9a48c8058e239a1
Henrik Burmann <henriabu@stud.ntnu.no>
2021-05-10 09:55:35 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module ntnu.idatt2001.henriabu.finalproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens ntnu.idatt2001.henriabu.finalproject to javafx.fxml;
+    exports ntnu.idatt2001.henriabu.finalproject;
+}
