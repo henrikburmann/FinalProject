d5db7681b32b527ad61d343ace78b6823882c197
Henrik Burmann <henriabu@stud.ntnu.no>
2021-05-10 10:49:40 +0200

    Basic gui made

--- src/main/java/ntnu/idatt2001/henriabu/finalproject/PostalCodeRegister.java
+++ src/main/java/ntnu/idatt2001/henriabu/finalproject/PostalCodeRegister.java
@@ -22,4 +22,8 @@ public class PostalCodeRegister {
      public void removePostalCode(PostalCode p){
         register.remove(p);
      }
+
+     public ObservableList<PostalCode> getRegister(){
+        return register;
+     }
 }
--- src/main/java/ntnu/idatt2001/henriabu/finalproject/PrimaryController.java
+++ src/main/java/ntnu/idatt2001/henriabu/finalproject/PrimaryController.java
@@ -1,12 +1,33 @@
 package ntnu.idatt2001.henriabu.finalproject;
 
 import java.io.IOException;
+
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class PrimaryController {
+    @FXML private TableView<PostalCode> tableView;
+    @FXML private TableColumn<PostalCode, String> codeColoumn;
+    @FXML private TableColumn<PostalCode, String> communeColoumn;
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        PostalCodeClient.setRoot("secondary");
+    private PostalCodeRegister postalCodeRegister = new PostalCodeRegister();
+
+
+    public void initialize(){
+        setCells();
+        PostalCode p = new PostalCode("2040", "Kløfta");
+        postalCodeRegister.addPostalCode(p);
+        tableView.setItems(getPostalCodes());
+    }
+    private void setCells(){
+        codeColoumn.setCellValueFactory(new PropertyValueFactory<>("code"));
+        communeColoumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+    }
+
+    private ObservableList<PostalCode> getPostalCodes(){
+        return postalCodeRegister.getRegister();
     }
 }
